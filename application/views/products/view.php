39a604e43df134d0986d5c5a960dc070750ff6cc
Marko Aleksic <psybaron@gmail.com>
2012-10-04 21:37:08 +0300

    v.1.0.0 Beta MK Release

--- application/views/products/view.php
+++ application/views/products/view.php
@@ -9,42 +9,35 @@
 		<a href="<?php echo site_url('products/delete/'.$master->id);?>" class="button" id="delete"><span class="delete">Бришење</span></a>
 	</div>
 <hr>
-	<dl>
-        <dt>Product:</dt>
-        <dd><?php echo $master->prodname;?></dd>
-        <dt>Code:</dt>
-        <dd><?php echo ($master->code!='')?$master->code:'-';?></dd>
-        <dt>Product Type:</dt>
-        <dd><?php echo $master->ptname;?></dd>
-		<dt>Product Category:</dt>
-        <dd><?php echo $master->pcname;?></dd>
-        <dt>Warehouse:</dt>
-        <dd><?php echo $master->wname;?></dd>
-        <dt>Base Unit:</dt>
-        <dd><?php echo $master->base_unit . ' ' . $master->uname;?></dd>
-        <dt>Retail Price:</dt>
-        <dd><?php echo $master->retail_price;?></dd>
-        <dt>Wholesale Price 1:</dt>
-        <dd><?php echo $master->whole_price1;?></dd>
-       <dt>Wholesale Price 2:</dt>
-        <dd><?php echo $master->whole_price2;?></dd>
-        <dt>Tax Rate:</dt>
-        <dd><?php echo $master->rate.'%';?></dd>
-        <dt>Commision:</dt>
-        <dd><?php echo $master->commision;?></dd>
-        <dt>Salable:</dt>
-        <dd><?php echo ($master->salable==1)?'Yes':'No';?></dd>
-       	<dt>Purchasable:</dt>
-        <dd><?php echo ($master->purchasable==1)?'Yes':'No';?></dd>
-        <dt>Stockable:</dt>
-        <dd><?php echo ($master->stockable==1)?'Yes':'No';?></dd>
-        <dt>Alert Qty.:</dt>
-        <dd><?php echo $master->alert_quantity. ' ' . $master->uname;?></dd>  
-        <dt>Status:</dt>
-        <dd><?php echo $master->status;?></dd>
-        <dt>Date of Entry:</dt>
-        <dd><?php echo $master->dateofentry;?></dd>   
-	</dl>
-
-<hr>
-<?php $this->load->view('includes/_del_dialog');?>
+<dl>
+    <dt>Назив:</dt>
+    <dd><?php echo $master->prodname;?></dd>
+    <dt>Код:</dt>
+    <dd><?php echo ($master->code!='')?$master->code:'-';?></dd>
+    <dt>Тип:</dt>
+    <dd><?php echo $master->ptname;?></dd>
+	<dt>Категорија:</dt>
+    <dd><?php echo $master->pcname;?></dd>
+    <dt>Магацин:</dt>
+    <dd><?php echo $master->wname;?></dd>
+    <dt>Основна Единица:</dt>
+    <dd><?php echo $master->base_unit . ' ' . $master->uname;?></dd>
+    <dt>МП Цена:</dt>
+    <dd><?php echo $master->retail_price;?></dd>
+    <dt>ГП Цена 1:</dt>
+    <dd><?php echo $master->whole_price1;?></dd>
+    <dt>ГП Цена 2:</dt>
+    <dd><?php echo $master->whole_price2;?></dd>
+    <dt>Данок (%):</dt>
+    <dd><?php echo $master->rate.'%';?></dd>
+    <dt>Провизија:</dt>
+    <dd><?php echo $master->commision;?></dd>
+    <dt>Се Продава:</dt>
+    <dd><?php echo ($master->salable==1)?'Да':'Не';?></dd>
+   	<dt>Се Купува:</dt>
+    <dd><?php echo ($master->purchasable==1)?'Да':'Не';?></dd>
+    <dt>Состојба:</dt>
+    <dd><?php echo ($master->stockable==1)?'Да':'Не';?></dd>
+    <dt>Мин Количина:</dt>
+    <dd><?php echo $master->alert_quantity. ' ' . $master->uname;?></dd>    
+</dl>
