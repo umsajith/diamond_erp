77d0178324c66f7dbeda27a47446c006e23b9c73
Marko Aleksic <psybaron@gmail.com>
2013-04-29 15:06:23 +0200

    added chevron arrow aside nav

--- application/views/includes/_sub_modules.php
+++ application/views/includes/_sub_modules.php
@@ -15,6 +15,9 @@
 
 			$link .= "?ref=sub_module&id={$sub_module->parent_id}";
 		?>
-		<li><?=anchor($link,$sub_module->title)?></li>
+		<li>
+			<a href="<?=site_url($link)?>"><?=$sub_module->title?>
+				<i class="icon-chevron-right aside-icon"></i></a>
+		</li>
 	<?php endforeach;?>
 </ul>
